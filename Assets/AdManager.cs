9661bb2d0c954e13abd1eb424edf6d7434442188
PhanHieu244 <phanhieu2443@gmail.com>
2024-09-25 22:25:28 +0700

    update

---
+++ Assets/AdManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdManager : MonoBehaviour
+{
+   
+}
---
+++ Assets/GDPR.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GDPR : MonoBehaviour
+{
+    public Button ShowPP;
+
+    public Button Accept;
+
+    public Button Cancle;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+}
